7af77b0d762e6fb3f3a14fda67678a1780caf88b
Anthony Voirol <anthonyvoirol47@gmail.com>
2025-11-20 15:46:42 +0100

    as

---
+++ assets/php/settings.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compte</title>
+</head>
+
+<body>
+    <section>
+        <h1>Paramètres</h1>
+        <button class="noButton">Compte</button>
+        <button class="noButton">Apparence</button>
+        <button class="noButton">Notification</button>
+    </section>
+    <section>
+
+    </section>
+</body>
+
+</html>
